987a6975ffede8aa1c6c98a130b8fd60a62d317c
KieuLinh0701 <uynukieulinh@gmail.com>
2025-04-17 21:20:09 +0700

    first

---
+++ app/src/main/java/vn/iotstar/bai10/MainActivity.java
@@ -0,0 +1,44 @@
+package vn.iotstar.bai10;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebViewClient;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import vn.iotstar.bai10.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        //getSupportActionBar().hide();
+        binding.webView.getSettings().setLoadWithOverviewMode(true);
+        binding.webView.getSettings().setUseWideViewPort(true);
+        binding.webView.getSettings().setJavaScriptEnabled(true);
+        binding.webView.setWebViewClient(new WebViewClient());
+        binding.webView.getSettings().setBuiltInZoomControls(true);
+        binding.webView.getSettings().setDomStorageEnabled(true);
+        binding.webView.getSettings().setDatabaseEnabled(true);
+        binding.webView.getSettings().setCacheMode (WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        binding.webView.setWebChromeClient(new WebChromeClient());
+        binding.webView.loadUrl("http://iotstar.vn/Account/Login");
+    }
+}
